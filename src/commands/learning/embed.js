4efc0e20bbcc19b12f33ef3b6ce8b7cdac09301e
Chris Kang <kangthchris@gmail.com>
2021-12-06 09:02:23 +1000

    Created embed.js

---
+++ src/commands/learning/embed.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('embed')
+    .setDescription('makes an embed!'),
+    async execute(interaction) {
+        const embed = new Discord.MessageEmbed();
+
+        embed.setTitle("This is a test embed")
+            .setAuthor(interaction.user.username, interaction.user.avatarURL({ dynamic: true }), "https://nhentai.net/g/349045/")
+            .setURL("https://nhentai.net/g/349045/")
+            .setDescription("this is some plain text, \n there is a link: [this is a test link](https://nhentai.net/g/349045/)")
+            .setColor("AQUA")
+            .setThumbnail(interaction.user.avatarURL({ dynamic: true }))
+            .addField("BBot Version", "1.0.0", true);
+
+        await interaction.reply({ embeds: [embed] });
+
+    }
+};
