9a4007c1cc5c3c6c90f91135b415bca8b7148fdc
Chris Kang <kangthchris@gmail.com>
2021-12-06 18:16:55 +1000

    Update playerlist.js

--- src/commands/game/playerlist.js
+++ src/commands/game/playerlist.js
@@ -1,15 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const database = require('../../database.js');
+const { MessageEmbed, Guild } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('playerlist')
         .setDescription('Shows list of players'),
     async execute(interaction) {
-        await interaction.reply('Showing all players...');
         const playerList = await database.Player.findAll({ attributes: ['name'] });
-        const playerString = playerList.map(p => p.name).join(', ') || 'No players set.';
+        const playerString = playerList.map(p => p.name).join('\n') || 'No players set.';
+        const embedNew = new MessageEmbed();
 
-        return interaction.editReply(`List of players: ${playerString}`);
-    },
+        embedNew.setTitle("Players List")
+            .setAuthor(interaction.user.username, interaction.user.avatarURL({ dynamic: true }))
+            .setDescription(`${playerString}`)
+            .setColor("AQUA")
+            .setThumbnail(interaction.user.avatarURL({ dynamic: true }))
+
+        await interaction.reply({ embeds: [embedNew] });
+    }
 };
