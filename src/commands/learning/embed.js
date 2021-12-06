241bdd5678f440255684192552e3bf56132516a9
Chris Kang <kangthchris@gmail.com>
2021-12-06 17:20:35 +1000

    Purified

--- src/commands/learning/embed.js
+++ src/commands/learning/embed.js
@@ -10,9 +10,8 @@ module.exports = {
         const embed = new MessageEmbed();
 
         embed.setTitle("This is a test embed")
-            .setAuthor(interaction.user.username, interaction.user.avatarURL({ dynamic: true }), "https://nhentai.net/g/349045/")
-            .setURL("https://nhentai.net/g/349045/")
-            .setDescription("this is some plain text, \n there is a link: [this is a test link](https://nhentai.net/g/349045/)")
+            .setAuthor(interaction.user.username, interaction.user.avatarURL({ dynamic: true }))
+            .setDescription("this is some plain text")
             .setColor("AQUA")
             .setThumbnail(interaction.user.avatarURL({ dynamic: true }))
             .addField("BBot Version", "1.0.0", true);
